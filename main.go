9476b7fc10602185e5c2bc50d3faf6120b9355ed
Eduarda de Castro Guterres <86486666+eduardacastro@users.noreply.github.com>
2023-03-06 13:14:30 -0300

    upload basic

---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+type BucketBasics struct {
+	S3Client *s3.Client
+}
+
+func main() {
+	log.Printf("Funcionou")
+	//BucketBasics.UploadFile(s3Client, "teste-golang-s3", "AKIAR6BYM7NK346M25X5", "teste.txt")
+	var (
+		s3Client BucketBasics
+		err      error
+	)
+
+	ctx := context.Background()
+	s3Client, err := initS3Client(ctx, "sa-east-1")
+	if err != nil {
+		fmt.Printf("initS3Client failed: %s", err)
+	}
+
+	sClient, err := CreateBucket(ctx, s3)
+}
+
+func initS3Client(ctx context.Context, region string) (*s3.Client, error) {
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		return nil, fmt.Errorf("impossivel conectar ao SDK config, %s", err)
+	}
+
+	return s3.NewFromConfig(cfg), nil
+}
+
+func (basics BucketBasics) ListBuckets() ([]types.Bucket, error) {
+	result, err := basics.S3Client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	var buckets []types.Bucket
+	if err != nil {
+		log.Printf("Couldn't list buckets for your account. Here's why: %v\n", err)
+	} else {
+		buckets = result.Buckets
+	}
+	return buckets, err
+}
+
+func (basics BucketBasics) CreateBucket(name string, region string) error {
+	_, err := basics.S3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{
+		Bucket: aws.String(name),
+		CreateBucketConfiguration: &types.CreateBucketConfiguration{
+			LocationConstraint: types.BucketLocationConstraint(region),
+		},
+	})
+	if err != nil {
+		log.Printf("Couldn't create bucket %v in Region %v. Here's why: %v\n",
+			name, region, err)
+	}
+	return err
+}
+
+func (basics BucketBasics) UploadFile(bucketName string, objectKey string, fileName string) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Printf("Couldn't open file %v to upload. Here's why: %v\n", fileName, err)
+	} else {
+		defer file.Close()
+		_, err := basics.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(objectKey),
+			Body:   file,
+		})
+		if err != nil {
+			log.Printf("Couldn't upload file %v to %v:%v. Here's why: %v\n",
+				fileName, bucketName, objectKey, err)
+		}
+	}
+	return err
+}
+
+func (basics BucketBasics) DownloadFile(bucketName string, objectKey string, fileName string) error {
+	result, err := basics.S3Client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	})
+	if err != nil {
+		log.Printf("Couldn't get object %v:%v. Here's why: %v\n", bucketName, objectKey, err)
+		return err
+	}
+	defer result.Body.Close()
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Printf("Couldn't create file %v. Here's why: %v\n", fileName, err)
+		return err
+	}
+	defer file.Close()
+	body, err := io.ReadAll(result.Body)
+	if err != nil {
+		log.Printf("Couldn't read object body from %v. Here's why: %v\n", objectKey, err)
+	}
+	_, err = file.Write(body)
+	return err
+}
+
+func (basics BucketBasics) CopyToFolder(bucketName string, objectKey string, folderName string) error {
+	_, err := basics.S3Client.CopyObject(context.TODO(), &s3.CopyObjectInput{
+		Bucket:     aws.String(bucketName),
+		CopySource: aws.String(fmt.Sprintf("%v/%v", bucketName, objectKey)),
+		Key:        aws.String(fmt.Sprintf("%v/%v", folderName, objectKey)),
+	})
+	if err != nil {
+		log.Printf("Couldn't copy object from %v:%v to %v:%v/%v. Here's why: %v\n",
+			bucketName, objectKey, bucketName, folderName, objectKey, err)
+	}
+	return err
+}
+
+func (basics BucketBasics) ListObjects(bucketName string) ([]types.Object, error) {
+	result, err := basics.S3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	})
+	var contents []types.Object
+	if err != nil {
+		log.Printf("Couldn't list objects in bucket %v. Here's why: %v\n", bucketName, err)
+	} else {
+		contents = result.Contents
+	}
+	return contents, err
+}
